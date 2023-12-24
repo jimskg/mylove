0bc10dfc1aa771541999254b1dddc094de8d0a15
jimskg <jimskg@yahoo.gr>
2023-12-24 06:09:02 +0200

    minor changes

--- mylove.js
+++ mylove.js
@@ -280,7 +280,7 @@
 
   // Set the date we're counting down to
   var endDate = new Date("Feb 6, 2024 00:00:00").getTime();
-  var endDate = new Date("Feb 6, 2023 00:00:00").getTime();
+  // var endDate = new Date("Feb 6, 2023 00:00:00").getTime();
   
   var x = setInterval(function() {
       var now = new Date().getTime();
